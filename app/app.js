59eb56dbeed53b3bb3ac25806eab321175f3c225
cloudaciodev <81725629+cloudaciodev@users.noreply.github.com>
2022-06-06 10:07:07 -0500

    Add rows sentence

--- app/app.js
+++ app/app.js
@@ -28,7 +28,7 @@ app.get('/', function(req, res) {
       console.log(err);
       return console.error('Error executing query', err.stack)
     }
- 
+       res.status(200).send(result.rows);
        res.status(200).send(result);
   });
 });
